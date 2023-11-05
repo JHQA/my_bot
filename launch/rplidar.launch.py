00790a06d015779e7a06c0816aa6477a926909a2
JHQA <JHQA@outlook.com>
2023-11-04 22:32:23 -0700

    added lidar launch file, channel type, baudrate

---
+++ launch/rplidar.launch.py
@@ -0,0 +1,23 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+
+        Node(
+            package='rplidar_ros',
+            executable='rplidar_node',
+            output='screen',
+            parameters=[{
+                'channel_type':'serial',
+                # 'serial_port': '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
+                'serial_baudrate': 115200,
+                'serial_port': '/dev/ttyUSB0',
+                'frame_id': 'laser_frame',
+                'angle_compensate': True,
+                'scan_mode': 'Standard'
+            }]
+        )
+    ])
